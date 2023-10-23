33d4162c7e0c23e119a1ca0fed571f7aff6fc453
Mayor2good <damoyeoluwamayowa@gmail.com>
2023-10-23 01:19:34 +0100

    First commit

---
+++ my-react-app/src/NotFound.js
@@ -0,0 +1,17 @@
+// NotFound.js
+import React from 'react';
+
+
+
+const NotFound = () => {
+  return (
+    <div>
+      <div>
+        <h1>404 - Not Found</h1>
+        <p>Sorry, the page you're looking for does not exist.</p>
+      </div>
+    </div>
+  );
+};
+
+export default NotFound;
